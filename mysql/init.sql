5ff4e9e41a28c51d047c475aa3f915ba0afaa5e2
markybaby <markgd0210@gmail.com>
2025-06-16 14:58:02 +0800

    updated coe-revalidation table attributes

--- mysql/init.sql
+++ mysql/init.sql
@@ -2,9 +2,11 @@ CREATE DATABASE IF NOT EXISTS mydb;
 USE mydb;
 
 CREATE TABLE IF NOT EXISTS coe-revalidation (
-    id INT PRIMARY KEY,
-    name VARCHAR(255),
-    price DECIMAL(10,2)
+    month VARCHAR(7),
+    type VARCHAR(10),
+    category VARCHAR(10),
+    number INT,
+    PRIMARY KEY (month, type, category)
 );
 
 CREATE TABLE IF NOT EXISTS coe-results (
