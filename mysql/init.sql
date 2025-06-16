d1ab7e2e76da860a0611ca6e6422e4ae2748576f
markybaby <markgd0210@gmail.com>
2025-06-16 15:06:13 +0800

    updated coe-results table attributes

--- mysql/init.sql
+++ mysql/init.sql
@@ -5,14 +5,19 @@ CREATE TABLE IF NOT EXISTS coe-revalidation (
     month VARCHAR(7),
     type VARCHAR(10),
     category VARCHAR(10),
-    number INT,
+    number_revalidated INT,
     PRIMARY KEY (month, type, category)
 );
 
 CREATE TABLE IF NOT EXISTS coe-results (
-    id INT PRIMARY KEY,
-    product_id INT,
-    quantity INT
+    month VARCHAR(7),
+    bidding_no INT,
+    category VARCHAR(10),
+    quota INT,
+    bids_success INT,
+    bids_received INT,
+    premium INT,
+    PRIMARY KEY (month, bidding_no, category)
 );
 
 CREATE TABLE IF NOT EXISTS monthly-analysis (
